5f26a38ac2169ca9dd9d03411138b89903dc6bc9
Rahul180206 <rahul.r2024aids@sece.ac.in>
2025-12-09 11:39:16 +0530

    Delete test_earphone_integration.py

--- test_earphone_integration.py
+++
@@ -1,7 +0,0 @@
-from earphone_workflow import EarphoneWorkflow
-import cv2
-
-ew = EarphoneWorkflow()  # reads .env
-img = cv2.imread("sample.jpg")  # use sample image in project root
-dets = ew.detect(img)
-print("Earphone detections:", dets)
